dc2f33ce4451206461d9b75f009f056e5e9bc0e1
Mariya <mariya.podosinova@gmail.com>
2020-04-30 20:15:50 +0100

    parsing data for the fullName

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 const url = "https://randomuser.me/api/";
 
-let btn = document.querySelector("#btn");
+let btn = document.querySelector("#btn"),
+  fullNameDisp = document.querySelector("#fullname");
 
 btn.addEventListener("click", function () {
   fetch(url)
     .then(function (res) {
-      console.log(res);
+      return res.json();
+    })
+    .then(function (data) {
+      let fullName =
+        data.results[0].name.first + " " + data.results[0].name.last;
+      fullNameDisp.innerHTML = fullName;
     })
     .catch(function (err) {
       console.log(err);
